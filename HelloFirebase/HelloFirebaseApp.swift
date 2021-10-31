f15919e018df6d8ec6dfead56a4f523d28db1325
IsYet1 <Don@IsYet.com>
2021-10-31 14:41:36 -0500

    Initial Commit

---
+++ HelloFirebase/HelloFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloFirebaseApp.swift
+//  HelloFirebase
+//
+//  Created by Don McKenzie on 10/31/21.
+//
+
+import SwiftUI
+
+@main
+struct HelloFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
