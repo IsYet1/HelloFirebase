639e8137dd074c4d15c54a4132c4c3819e303319
IsYet1 <Don@IsYet.com>
2021-11-03 18:38:09 -0500

    Builds and runs. Using the Swift package manager.

--- HelloFirebase/ContentView.swift
+++ HelloFirebase/ContentView.swift
@@ -7,11 +7,38 @@
 
 import SwiftUI
 import Firebase
+import FirebaseFirestoreSwift
+import simd
 
 struct ContentView: View {
+    private var db: Firestore
+    @State private var title: String = ""
+    
+    init() {
+        db = Firestore.firestore()
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            TextField("Enter Task", text: $title)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            Button("Save") {
+                let task = Task(title: title)
+                do {
+                    _ = try db.collection("tasks").addDocument(from: task) {err in
+                        if let err = err {
+                            print(err.localizedDescription)
+                        } else {
+                            print("Document has been saved")
+                        }
+                    }
+                } catch let error {
+                    print(error.localizedDescription)
+                }
+                //                db.collection("tasks").addDocument(data: ["Test": "Data"])
+            }
+            Spacer()
+        }.padding()
     }
 }
 
---
+++ HelloFirebase/Models/Task.swift
@@ -0,0 +1,12 @@
+//
+//  Task.swift
+//  HelloFirebase
+//
+//  Created by Don McKenzie on 11/3/21.
+//
+
+import Foundation
+
+struct Task: Codable {
+    let title: String
+}
